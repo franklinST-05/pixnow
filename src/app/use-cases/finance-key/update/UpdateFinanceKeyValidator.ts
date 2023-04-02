b68fa4fe86c8df34a324e73659000e112796f5d7
franklinST-05 <franklinteixeira205@gmail.com>
2023-04-02 11:08:59 -0300

    refactor: added optional attributes for update FinanceKey

--- src/app/use-cases/finance-key/update/UpdateFinanceKeyValidator.ts
+++ src/app/use-cases/finance-key/update/UpdateFinanceKeyValidator.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const UpdateFinanceKeyValidator = z.object({
     id: z.string().uuid(),
-    description: z.string(),
-    key: z.string(),
+    description: z.string().optional(),
+    key: z.string().optional(),
 });
 
 export type UpdateFinanceKeyType = z.infer<typeof UpdateFinanceKeyValidator>
